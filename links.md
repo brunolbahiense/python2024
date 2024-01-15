281f0237716769a8119964bea7e6ab36eaca512e
brunolbahiense <brunoluisbahiense@gmail.com>
2024-01-15 12:44:59 -0300

    def functions

---
+++ links.md
@@ -0,0 +1,2 @@
+# Links úteis:
+1. Para textos no terminal: https://fsymbols.com/
--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,8 +1,36 @@
-print('Sabor Express \n')
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante ')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+import os
+def nome_programa():
+    print('𝙎𝙖𝙗𝙤𝙧 𝙀𝙭𝙥𝙧𝙚𝙨𝙨 🍜 \n')
+    
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante ')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print('Opção escolhida', opcao_escolhida)
+# def é a forma de declarar funções
+def finalizar():
+    os.system('cls')
+    print('Sair')
+
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    elif opcao_escolhida == 4:
+        finalizar()
+    else:
+        print(f'opcao {opcao_escolhida} invalida!')
+
+def main():
+    nome_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
